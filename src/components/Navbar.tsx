e0d0d9a48e61b386df529457794a33763cf9645c
ArturPawelski <arturpawelski13@gmail.com>
2023-05-02 21:21:25 +0200

    project setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+      <p>navbar</p>
+      navbar
+    </div>
+  );
+};
+
+export default Navbar;
