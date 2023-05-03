1d073600061f69b0399b27cfa26365057a325ccf
ArturPawelski <arturpawelski13@gmail.com>
2023-05-03 23:03:22 +0200

    burger menu responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import React, { useState } from 'react';
+import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineClose } from 'react-icons/ai';
 import { GiHamburgerMenu } from 'react-icons/gi';
 const Navbar = () => {
+  const [menu, setMenu] = useState<boolean>(true);
+
+  const toggleMenu = (): void => {
+    setMenu(!menu);
+  };
+
   return (
     <div className='text-white font-quicksand  w-full '>
       <header className='py-8 lg:px-20 md:px-4'>
-        <GiHamburgerMenu className=' absolute left-4 top-[38px] md:hidden' size={25} />
-        <h1 className='font-bold text-3xl md:text-6xl text-center mx-auto'>GuitarLab</h1>
+        {menu ? (
+          <GiHamburgerMenu
+            onClick={toggleMenu}
+            className=' absolute left-4 sm:left-8 top-[38px] md:hidden z-40 cursor-pointer hover:scale-[1.1] transition duration-500 active:translate-y-4 '
+            size={25}
+          />
+        ) : (
+          <AiOutlineClose
+            onClick={toggleMenu}
+            className=' absolute left-4 sm:left-8 top-[38px] md:hidden z-40 cursor-pointer hover:scale-[1.1] transition duration-500 active:translate-y-4 '
+            size={25}
+          />
+        )}
+
+        <h1 className=' font-bold text-3xl md:text-6xl text-center mx-auto  '>GuitarLab</h1>
+        <AiOutlineShoppingCart className=' absolute right-4 top-[38px] md:hidden' size={25} />
 
         <nav className=' hidden md:flex items-center justify-between relative md:bottom-12'>
           <div className='flex items-center lg:gap-4 '>
@@ -24,7 +44,19 @@ const Navbar = () => {
         <li>INFORMATIONS</li>
       </ul>
 
-      <ul className=' md:hidden'></ul>
+      {!menu && (
+        <nav className='fixed left-0 top-[0%] h-full w-[40%] bg-[#0C0C0C]  bg-opacity-0 z-0 md:hidden'>
+          <ul className=' flex flex-col gap-8 px-4 sm:px-8 mt-24   '>
+            <li className='border-b py-2 w-full min-w-[140px]'>NEW</li>
+            <li className='border-b py-2 w-full min-w-[140px]'>PRODUCTS</li>
+            <li className='border-b py-2 w-full min-w-[140px]'>PERSONALIZE</li>
+            <li className='border-b py-2 w-full min-w-[140px]'> INFORMATIONS</li>
+            <div className='flex items-center lg:gap-4 '>
+              <input type='text' placeholder='search...' className='py-2 px-2 max-w-[130px] text-white rounded-3xl bg-[#0C0C0C] border-[1px] border-white focus:outline-none' />
+            </div>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
