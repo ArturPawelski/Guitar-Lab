03642b00b5dca15e27403a66c685504f1d9f4538
ArturPawelski <arturpawelski13@gmail.com>
2023-05-03 23:13:10 +0200

    adding tabindex

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,32 +16,34 @@ const Navbar = () => {
             onClick={toggleMenu}
             className=' absolute left-4 sm:left-8 top-[38px] md:hidden z-40 cursor-pointer hover:scale-[1.1] transition duration-500 active:translate-y-4 '
             size={25}
+            tabIndex={0}
           />
         ) : (
           <AiOutlineClose
             onClick={toggleMenu}
             className=' absolute left-4 sm:left-8 top-[38px] md:hidden z-40 cursor-pointer hover:scale-[1.1] transition duration-500 active:translate-y-4 '
             size={25}
+            tabIndex={0}
           />
         )}
 
         <h1 className=' font-bold text-3xl md:text-6xl text-center mx-auto  '>GuitarLab</h1>
-        <AiOutlineShoppingCart className=' absolute right-4 top-[38px] md:hidden' size={25} />
+        <AiOutlineShoppingCart className=' absolute right-4 top-[38px] md:hidden  cursor-pointer hover:scale-[1.1] transition duration-500 active:translate-y-4' size={25} />
 
         <nav className=' hidden md:flex items-center justify-between relative md:bottom-12'>
           <div className='flex items-center lg:gap-4 '>
             <input type='text' placeholder='search...' className='py-2 px-2 text-white rounded-3xl bg-[#0C0C0C] border-[1px] border-white focus:outline-none' />
             <AiOutlineSearch size={30} />
           </div>
-          <AiOutlineShoppingCart size={30} />
+          <AiOutlineShoppingCart size={30} tabIndex={0} className=' cursor-pointer hover:scale-[1.1] transition duration-500  ' />
         </nav>
       </header>
 
-      <ul className=' hidden md:flex justify-center text-lg font-thin mt-[-20px] py-4 gap-16 w-full border-t-[1px] border-b-[1px]'>
-        <li>NEW</li>
-        <li>PRODUCTS</li>
-        <li>PERSONALIZE</li>
-        <li>INFORMATIONS</li>
+      <ul className=' hidden md:flex justify-center text-lg font-thin mt-[-30px] py-4 gap-16 w-full border-t-[1px] border-b-[1px]'>
+        <button>NEW</button>
+        <button>PRODUCTS</button>
+        <button>PERSONALIZE</button>
+        <button>INFORMATIONS</button>
       </ul>
 
       {!menu && (
