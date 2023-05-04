d3f41062d0437bd6fda835148a89de0a853c9103
ArturPawelski <arturpawelski13@gmail.com>
2023-05-04 21:33:09 +0200

    h1 h2 css properties

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,15 @@ import MainRedGuitar from './images/main-guitar-red.png';
 const Header = () => {
   return (
     <main className='relative text-white font-quicksand w-full bg-footer-texture bg-cover bg-center '>
-      <h1 className='hidden sm:block sm:absolute md:top-[0px] md:left-[0%] lg:left-[5%] xl:left-[12.5%] 2xl:left-[15%] md:text-4xl  max-w-[400px]'>A new generation of guitars is coming...</h1>
+      <h1 className='hidden uppercase md:absolute md:block md:text-4xl md:font-bold  md:top-[0px] md:left-[3%] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%]   max-w-[420px]'>
+        A new generation of guitars is coming...
+      </h1>
+
       <div className='flex mt-16 justify-center items-center min-h-screen'>
         <img src={MainRedGuitar} alt='new guitar ' className='max-w-full max-h-full' />
       </div>
-      <h2 className='hidden sm:block sm:absolute md:top-[400px] md:right-[0%] lg:right-[5%] xl:right-[12.5%] 2xl:right-[15%]  md:text-3xl text-right  max-w-[400px]'>
+
+      <h2 className='hidden text-right font-[300] md:block md:text-3xl sm:absolute md:top-[400px] md:right-[3%] lg:right-[5%] xl:right-[12.5%] 2xl:right-[17%]    md:max-w-[300px]  lg:max-w-[400px]'>
         Our store is proud to present some of the most innovative guitars on the market
       </h2>
       <h3 className='absolute top-40 left-0'>Whether you're a beginner or a seasoned pro, our selection of guitars has something for everyone. </h3>
