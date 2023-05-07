462b972bf7da6b7899d9e881e001b73b26a7084c
ArturPawelski <arturpawelski13@gmail.com>
2023-05-07 20:41:18 +0200

    h2 styles for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,10 +13,10 @@ const Header = () => {
         <img src={MainRedGuitar} alt='new guitar ' className=' max-w-full max-h-full' />
       </div>
 
-      <h2 className='hidden text-right font-[300] lg:block md:text-3xl sm:absolute md:top-[400px]  lg:right-[5%] xl:right-[12.5%] 2xl:right-[17%]    md:max-w-[300px]  lg:max-w-[400px]'>
+      <h2 className='text-center text-xl mt-[-20%] mx-auto max-w-[400px] font-[300] sm:mt-[-25%]  sm:text-3xl   sm:max-w-[500px] md:mt-[-28%] md:top-[400px] lg:mt-0 lg:absolute lg:block lg:text-right lg:max-w-[400px] lg:right-[5%] xl:right-[12.5%] 2xl:right-[17%] '>
         Our store is proud to present some of the most innovative guitars on the market
       </h2>
-      <h3 className='hidden  font-[400] lg:block md:text-3xl sm:absolute md:top-[800px] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%]    md:max-w-[300px]  lg:max-w-[400px]'>
+      <h3 className='  lg:block lg:text-3xl lg:max-w-[400px] lg:absolute lg:top-[800px] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%] '>
         Whether you're a beginner or a seasoned pro, our selection of guitars has something for everyone.{' '}
       </h3>
       <h4 className='hidden text-right font-[300] lg:block md:text-3xl sm:absolute  lg:top-[1350px]  lg:right-[4.5%] xl:right-[10.5%] 2xl:right-[16%]    md:max-w-[300px]  lg:max-w-[250px] xl:max-w-[300px]'>
