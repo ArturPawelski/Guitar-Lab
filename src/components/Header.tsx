375c99c4f9895ccadac333c056ffc8a8f549ce25
ArturPawelski <arturpawelski13@gmail.com>
2023-05-07 21:02:15 +0200

    h4 and h5 styles for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,16 +13,16 @@ const Header = () => {
         <img src={MainRedGuitar} alt='new guitar ' className=' max-w-full max-h-full' />
       </div>
 
-      <h2 className='text-center text-xl mt-[-20%] mx-auto max-w-[400px] font-[500] sm:mt-[-25%]  sm:text-3xl   sm:max-w-[500px] md:mt-[-28%] md:top-[400px] lg:mt-0 lg:absolute lg:block lg:text-right lg:max-w-[400px] lg:right-[5%] xl:right-[12.5%] 2xl:right-[17%] '>
+      <h2 className='text-center text-xl mt-[-20%] mx-auto max-w-[400px] font-[500] sm:mt-[-25%]  sm:text-3xl   sm:max-w-[500px] md:mt-[-28%] md:top-[400px] lg:font-[300] lg:mt-0 lg:absolute lg:block lg:text-right lg:max-w-[400px] lg:right-[5%] xl:right-[12.5%] 2xl:right-[17%] '>
         Our store is proud to present some of the most innovative guitars on the market
       </h2>
-      <h3 className='text-xl font-[300] text-center max-w-[400px]  mx-auto mt-16 sm:text-2xl sm:max-w-[600px] lg:mt-0 lg:block lg:text-3xl lg:max-w-[400px] lg:absolute lg:top-[800px] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%]   '>
+      <h3 className='text-xl font-[300] text-center max-w-[400px]  mx-auto mt-20 sm:text-2xl sm:max-w-[600px] lg:mt-0 lg:block lg:text-3xl lg:max-w-[400px] lg:absolute lg:top-[800px] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%]   '>
         Whether you're a beginner or a seasoned pro, our selection of guitars has something for everyone.{' '}
       </h3>
-      <h4 className='hidden text-right font-[300] lg:block md:text-3xl sm:absolute  lg:top-[1350px]  lg:right-[4.5%] xl:right-[10.5%] 2xl:right-[16%]    md:max-w-[300px]  lg:max-w-[250px] xl:max-w-[300px]'>
+      <h4 className='text-xl font-[700] text-center max-w-[400px]  mx-auto mt-20 sm:text-2xl sm:max-w-[600px]  lg:block lg:text-3xl lg:absolute lg:text-right lg:font-[300] lg:top-[1350px]  lg:right-[4.5%]  lg:max-w-[250px] xl:right-[10.5%]  xl:max-w-[300px] 2xl:right-[16%]'>
         CHOOSE your guitar and go into the world!
       </h4>
-      <h5 className='hidden  font-[400] lg:block md:text-3xl sm:absolute md:top-[1830px] lg:left-[5%] xl:left-[12.5%] 2xl:left-[17%]    md:max-w-[300px]  lg:max-w-[400px]'>
+      <h5 className='text-xl font-[300] text-center max-w-[400px]  mx-auto mt-20 sm:text-2xl sm:max-w-[600px] lg:font-[300] lg:block lg:text-3xl lg:absolute lg:top-[1830px] lg:left-[5%] lg:max-w-[400px] xl:left-[12.5%] 2xl:left-[17%] '>
         PERSONALIZE your own guitar that will capture your heart and the hearts of others
       </h5>
     </main>
