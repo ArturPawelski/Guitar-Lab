1b078600a885f86cfc0e06063a97d58faa34c7b7
ArturPawelski <arturpawelski13@gmail.com>
2023-05-17 20:38:24 +0200

    adding useCallback and react-types

--- src/components/Personalize.tsx
+++ src/components/Personalize.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GuitarPhoto from './images/guitar-personalize.png';
 import { motion } from 'framer-motion';
-
+import Typed from 'react-typed';
 const Personalize = () => {
   return (
     <motion.main
@@ -14,7 +14,9 @@ const Personalize = () => {
     >
       <div className='max-w-[95%] mx-auto sm:max-w-[85%] md:max-w-[80%] md:flex  md:max-h-[600px]'>
         <motion.section initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: false }} transition={{ delay: 0.6, duration: 0.5 }}>
-          <h1 className='uppercase text-4xl font-bold text-center md:text-left  '>personalize</h1>
+          <h1 className='uppercase text-4xl font-bold text-center md:text-left  '>
+            <Typed backSpeed={100} typeSpeed={100} loop strings={['personalize']} />
+          </h1>
           <p className='font-[300] text-center mt-12 text-sm md:text-xl md:max-w-[380px] md:text-left lg:max-w-[480px] lg:mt-20 lg:text-x;  xl:max-w-[570px] 2xl:max-w-[700px] '>
             At our shop, we offer custom-made guitars tailored to your unique preferences and specifications. Our team of skilled specialists has the expertise to create a one-of-a-kind instrument
             that perfectly suits your playing style and musical taste. Whether you're looking for a specific wood type, pickup configuration, or other custom features, we can work with you to bring
